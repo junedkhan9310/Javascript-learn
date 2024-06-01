45c95f3dbe05ce72e7da7889a5103dce049c8bac
junedkhanfarh <junedkhanfarh@gmail.com>
2024-06-01 20:35:03 +0530

    added number opearotrs

---
+++ num_maths.js
@@ -0,0 +1,10 @@
+const score = 500
+
+const age = new Number(12)
+// console.log(age)
+
+// console.log(age.toString().length); 
+console.log(age.toFixed(99)); //floating point me represetn karta
+
+const othernum = 2113.464
+console.log(othernum.toPrecision(3));
