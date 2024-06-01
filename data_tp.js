d47d13e53821b734011d3781c78b33af4de815d7
junedkhanfarh <junedkhanfarh@gmail.com>
2024-06-01 15:35:43 +0530

    added two files of var and data types

---
+++ data_tp.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3)  we are using node js not browser 
+
+console.log(3+3);
+
+console.log("juned")
+
+let bool = false;
+
+console.log(bool)
+
+let state; //undefined
+
+let game = null //null 
+
+console.log(state)
+let age=123;
+console.log(game)
+console.log(typeof bool)
+
+console.log(typeof undefined)  //undefined hi ata 
+console.log(typeof null)  //objects
+
+
+//symbol for uniqeness
+
+//object
+
+
+
+
---
+++ var_cons.js
@@ -0,0 +1,14 @@
+const accountId= 12321;  //const can't cange
+
+let accountEmail = "juned@gmail.com"  
+var accountPassword = "123"  /* use 'let' only not 'var' */
+
+accountCity = "jaipur"
+let accountstate;
+// accountId=2 
+
+accountEmail= "sc@.com"
+accountPassword= "7869"
+accountCity="muba"
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountstate])
