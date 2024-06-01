dff5e30b2b5159efd4866f848df273bf8877e1d6
junedkhanfarh <junedkhanfarh@gmail.com>
2024-06-01 16:08:11 +0530

    added conversation

---
+++ convo.js
@@ -0,0 +1,24 @@
+let score = "string"
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)  //nan mean not a number
+
+
+
+// "33" => 33
+//"33abs"=> Nan
+// true => 1; false =>0
+
+let login  =1; 
+
+let boolloging = Boolean(login);
+
+console.log(typeof(boolloging))
+console.log(boolloging) 
+// true => 1; false =>0
+// ""=> false 
+// "june" => true
