f0fbf3c1e26bf8690b3d76e9db73b2bb2847a509
junedkhanfarh <junedkhanfarh@gmail.com>
2024-06-01 16:41:36 +0530

    added comparing and basic operator

---
+++ compare.js
@@ -0,0 +1,13 @@
+
+// console.log("2"<3)
+
+
+console.log(0 == 0)
+
+console.log(null<0)
+
+console.log(null >= 0)
+
+//strict check ====
+
+console.log("2"===2);
---
+++ operation.js
@@ -0,0 +1,10 @@
+let num=2
+let negval = -num
+console.log(negval)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
