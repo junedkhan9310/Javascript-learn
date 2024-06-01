760e66b2426a44d65126943ee1aea336e22a9c6c
junedkhanfarh <junedkhanfarh@gmail.com>
2024-06-01 20:08:36 +0530

    strings functions

---
+++ strings.js
@@ -0,0 +1,23 @@
+const game = new String('Callofduty')
+
+console.log(game)
+
+console.log(game[1]);
+
+// for (let i =(game.length-1);i>=0;i--){
+//     console.log(game[i]);
+// }
+
+console.log(game.toUpperCase());
+
+console.log(game.charAt(6));
+
+console.log(game.indexOf('u'));
+
+
+
+
+
+
+
+
