f40a2b579d9b7cba24cea3823578e4bdeff1cb5b
junedkhanfarh <junedkhanfarh@gmail.com>
2024-06-02 15:05:31 +0530

    some number thing like min max and forumla for randome

--- num_maths.js
+++ num_maths.js
@@ -4,7 +4,19 @@ const age = new Number(12)
 // console.log(age)
 
 // console.log(age.toString().length); 
-console.log(age.toFixed(99)); //floating point me represetn karta
+// console.log(age.toFixed(99)); //floating point me represetn karta
 
 const othernum = 2113.464
-console.log(othernum.toPrecision(3));
+// console.log(othernum.toPrecision(3));
+
+// console.log(Math.random());
+
+// console.log(Math.random()*10 +1);
+console.log((Math.random()*10 )+1);
+
+console.log(Math.floor(Math.random()*10)+1)
+
+const min =10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
