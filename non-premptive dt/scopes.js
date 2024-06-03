58f05905ee8f376f072ce5ab18dbf5b134a01cfe
junedkhanfarh <junedkhanfarh@gmail.com>
2024-06-03 20:18:53 +0530

    scopes

---
+++ non-premptive dt/scopes.js
@@ -0,0 +1,15 @@
+
+// console.log(a,b,c);
+
+// {} --> scope 
+
+if(true){
+    let a=10
+    const b=20
+    var c=32   
+
+}
+
+// console.log(a);  it is declared in scope so we cna't use
+// console.log(b);  it is also
+console.log(c);  //this can be use due to var this is disadvatage of 'var'
