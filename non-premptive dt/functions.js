a1658e60e069057c6416e938c52dd879fe08501d
junedkhanfarh <junedkhanfarh@gmail.com>
2024-06-03 17:46:12 +0530

    learned the fuction in js and it's relation with objects and how t ocal lit

---
+++ non-premptive dt/functions.js
@@ -0,0 +1,102 @@
+
+function sayname(){
+    console.log("j");
+    console.log("u");
+    console.log("n");
+    console.log("e");
+    console.log("d");
+}
+
+// sayname()
+
+// function addnum(number1,number2){
+//     console.log(number1+number2)
+// }
+
+function addnum(number1,number2){
+    let result = number1+number2;
+    return result
+}
+
+let result= addnum(32,56)
+// console.log(result);
+// addnum(22,12)
+
+
+function loginusermess(username){
+    if(!username){ //equivalent to username===undefined
+        console.log("please enter username");
+        return ;
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginusermess("juned"));
+
+// console.log(loginusermess());
+
+//**********************function wit obj and arrray */
+
+function calcu_cartprice(...num1){//agar bohot sari value pgi to
+    return num1  //it'll return array of number
+
+}
+
+
+// console.log(calcu_cartprice(23,75,343,56,74));
+
+
+//***************Now using object in function
+
+const user={
+    name: "juned",
+    age :18
+
+}
+
+function userincompany(anybody){
+    console.log(`name of user is ${anybody.name} and age is ${anybody.age}`)
+}
+
+// userincompany(user) we can also pass object directly
+
+userincompany({
+    name: "naed",
+    age : 232
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
